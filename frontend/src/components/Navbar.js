4d2782bb83b78ff1a3d55ad9a7fd0dac98c34eb6
jsm1306 <sindhu.j1729@gmail.com>
2025-03-23 17:08:06 +0530

    Experiment-4 done

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h2>Workout buddy</h2>
+        </Link>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
