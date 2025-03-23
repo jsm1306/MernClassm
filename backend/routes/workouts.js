6dab06329e0840473996141e8067d9e16bfa964d
jsm1306 <sindhu.j1729@gmail.com>
2025-03-23 16:43:23 +0530

    Experiment-3 done

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,19 +1,15 @@
 import express from "express";
-import Workout from "../models/workoutModel.js";
+import {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} from "../controllers/workoutController.js";
 const router = express.Router();
-
-router.get("/", async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 });
-  res.status(200).json(workouts);
-});
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.get("/", getWorkouts);
+router.get("/:id", getWorkout);
+router.post("/", createWorkout);
+router.delete("/:id", deleteWorkout);
+router.patch("/:id", updateWorkout);
 export default router;
